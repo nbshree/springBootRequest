18c0766142ffad0499272fbbb60e58204bc41117
shree <401163814@qq.com>
2020-05-06 15:52:08 +0800

    登陆接口

--- src/main/java/com/request/controller/api/HomeController.java
+++ src/main/java/com/request/controller/api/HomeController.java
@@ -1,25 +1,38 @@
 package com.request.controller.api;
 
+import com.request.service.LoginService;
+import com.request.vo.result.RestResult;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
+
 @RestController
 public class HomeController {
-
+    @Resource
+    LoginService loginService;
     @RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
-    public String login(@RequestParam String userName, @RequestParam String userPwd) {
-        try {
+    public RestResult login(@RequestParam String userName, @RequestParam String userPwd) {
             // Shiro登录
             Subject subject = SecurityUtils.getSubject();
             UsernamePasswordToken token = new UsernamePasswordToken(userName, userPwd);
-            subject.login(token);
-            subject.getSession().setAttribute("user", userName);
-            return "登陆成功";
-        } catch (Exception ex) {
-            return "登陆失败" + ex;
-        }
+            try {
+                //进行验证，这里可以捕获异常，然后返回对应信息
+                subject.login(token);
+//            subject.checkRole("admin");
+//            subject.checkPermissions("query", "add");
+            } catch (AuthenticationException e) {
+                e.printStackTrace();
+                return RestResult.createFailedResult("账号或密码错误！");
+            } catch (AuthorizationException e) {
+                e.printStackTrace();
+                return RestResult.createFailedResult("没有权限");
+            }
+            return RestResult.createSuccessResult("200", loginService.getUserByName(userName),subject.getSession().getId().toString());
     }
 
     @RequestMapping(value = "/logoff", method = {RequestMethod.GET, RequestMethod.POST})
